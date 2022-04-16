cad04f6a280c49c39f2a9f98543f953626ab0675
Emily Schell <lauraemilyschell@gmail.com>
2022-04-16 13:36:55 -0400

    combines books and hello_world routes, adds error handling

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,41 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
+
+hello_world_bp = Blueprint("hello_world_bp", __name__)
+
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    return "Hello, World!"
+
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
+
 
 class Book:
     def __init__(self, id, title, description):
         self.id = id
         self.title = title
         self.description = description
+
 
 books = [
     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
@@ -14,6 +44,18 @@ books = [
 ]
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+    for book in books:
+        if book.id == book_id:
+            return book
+    abort(make_response({"message": f"book {book_id} not found"}, 404))
+
 
 @books_bp.route("", methods=["GET"])
 def handle_books():
@@ -27,3 +69,13 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
